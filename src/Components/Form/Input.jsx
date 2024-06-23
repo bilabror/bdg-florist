6622d45c871ad5afaf9e4d0626db24978ee2300c
strayneko <bayurendi10@gmail.com>
2024-06-23 17:01:54 +0700

    Refac: move rest to the end of the component

--- src/Components/Form/Input.jsx
+++ src/Components/Form/Input.jsx
@@ -3,7 +3,7 @@ import {twMerge} from "tailwind-merge";
 export default function Input({ className, ...rest }) {
     return (
         <>
-            <input type="text" {...rest} placeholder="Enter your name" className={twMerge(`rounded-lg placeholder:font-medium py-3 px-5 placeholder:text-gray-300 border border-gray-300 ${className}`)} />
+        <input type="text" className={twMerge(`rounded-lg placeholder:font-medium py-3 px-5 placeholder:text-gray-300 border border-gray-300 ${className || ''}`)} {...rest} />
         </>
     )
 }
