c3050c27b2842cede38fde2a724a30ae4ff03ea8
strayneko <bayurendi10@gmail.com>
2024-06-23 17:02:53 +0700

    Refac: use or operator to avoid undefined class

--- src/Components/Form/Button.jsx
+++ src/Components/Form/Button.jsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 export default function Button({ className, children, ...rest }) {
     return (
         <>
-            <button className={twMerge(`bg-primary text-white rounded-lg px-6 py-2 ${className}`)} {...rest}>{children}</button>
+            <button className={twMerge(`bg-primary text-white rounded-lg px-6 py-2 ${className || ''}`)} {...rest}>{children}</button>
         </>
     )
 }
